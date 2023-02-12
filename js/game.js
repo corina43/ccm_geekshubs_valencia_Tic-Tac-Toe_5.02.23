dd2586ed771074ccc1d2ffbcf60a67303c8af3da
corina <ciorpaccorina@gmail.com>
2023-02-12 22:41:59 +0100

    conseguido cambio de ficha

--- js/game.js
+++ js/game.js
@@ -1,28 +1,22 @@
 
-//capturar datos de storage
+// obtener los nombres de los jugadores desde el almacenamiento de sesión.
 let inputNamePlayer1  = sessionStorage.getItem("namePlayer1");
+
 let inputNamePlayer2  = sessionStorage.getItem("namePlayer2");
 
 document.getElementById("player1").innerHTML = inputNamePlayer1;
 document.getElementById("player2").innerHTML = inputNamePlayer2;
-
-
-
-
-
-
+// arreglo con todas las celdas que representan el juego de tres en raya
 let tablero = Array.from(document.getElementsByClassName("box"));
-
+// booleana que determina de quién es el turno actual, al inicio es true lo que significa que el jugador X tiene el primer turno.
 let turno = true;
-
-let namePlayer1 = 3;
-let namePlayer2 = 3;
-
-
+// arreglo que declaro el estado tablero
 let miTablero = ["","","","","","","","",""];
 
-let combinacionGanadora = [
-    [0, 1, 2],
+let celdasJugador1 = [];
+let celdasJugador2= [];
+// arreglo de arreglos que representan las combinaciones ganadoras en el juego de tres en raya
+let combinacionGanadora = [    [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
     [0, 3, 6],
@@ -30,47 +24,39 @@ let combinacionGanadora = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
-  ];
-
+];
+// función que muestra en la consola el estado  del tablero
 const comprueboGanador = () => {
     console.log(miTablero);
-
-}
-
+};
+// bucle que recorre todas las celdas y agrega un evento de clic a cada una. Cuando una celda es clicada
 tablero.map((celda) => {
-        celda.addEventListener('click', ()=> {
-
-        
-            celda.classList.add('box');
-
-         
-
-       
-            if((celda.innerHTML === "")) {
-               if(namePlayer1 > 0 || namePlayer2 > 0){
-
-                                                      
-
-        celda.innerHTML = (turno) ? "X" : "O";//Aqui PINTO el simbolo, dependiendo del turno
-
-
-                (turno) ? namePlayer1 -- :  namePlayer2--;//                 //Después, dependiendo tambien de quien fuese el turno, le quito una ficha
-                //de las que puede poner
-
-
-           
-                miTablero[celda.id] = (turno) ? "X" : "O";      //Finalmente, además de que en pantalla quede marcada, guardo la posición 
-                 //que ocupa en mi tablero lógico
-
-               comprueboGanador();
-
-                 //Cambiamos de turno
-                turno = !turno;}
-           }else{
-               celda.innerHTML = (turno) ? "" : "";
-               
-               (turno) ? namePlayer1 ++ : namePlayer2++;
-           }
-        })
-    }
-)
+    celda.addEventListener('click', () => {
+        celda.classList.add('box');
+// Si la celda está vacía, se pone X o O en ella dependiendo del turno actual
+        if(celda.innerHTML === "") {
+            celda.innerHTML = (turno) ? "X" : "O";
+//actualizo el arreglo
+            miTablero[celda.id] = (turno) ? "X" : "O";
+ // Si el turno es del jugador 1, se agrega la celda a celdasJugador1 y si se han jugado más de 3 celdas, se elimina la primera celda de celdasJugador1
+            if (turno) {
+                if (celdasJugador1.length >= 3) {
+                    let primeraCelda = celdasJugador1.shift();
+                    primeraCelda.innerHTML = "";
+                }
+                celdasJugador1.push(celda);
+//si no al revers
+            } else {
+                if (celdasJugador2.length >= 3) {
+                    let primeraCelda = celdasJugador2.shift();
+                    primeraCelda.innerHTML = "";
+                }
+                celdasJugador2.push(celda);
+            }
+// verifico ganador
+            comprueboGanador();
+// cambio turno
+            turno = !turno;
+        }
+    });
+});
